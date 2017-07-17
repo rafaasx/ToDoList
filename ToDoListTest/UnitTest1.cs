61b9180d71da440713369d5332ab1aabf8c489b8
Rafael Xavier <rafaeltwisted@gmail.com>
2017-07-17 16:51:12 -0300

    Unit Tests

---
+++ ToDoListTest/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ToDoListTest
+{
+    [TestClass]
+    public class TarefaTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var testProducts = GetTestProducts();
+            var tarefas
+            var controller = new SimpleProductController(testProducts);
+
+            var result = controller.GetAllProducts() as List<Product>;
+            Assert.AreEqual(testProducts.Count, result.Count);
+        }
+    }
+    }
+}
